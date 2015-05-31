efcf7a1dc8c762e6867aaa86ae3b332976d9fe60
Lucas <lucas@agtlucas.com>
2015-05-30 23:43:01 -0300

    Adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install nginx
+sudo service nginx start
